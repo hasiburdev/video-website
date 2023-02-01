9b027ef9fc614b78757da92c1e1f0bf2c4dfb2f0
Hasibur Rahman <hasiburdev@gmail.com>
2023-02-01 16:12:31 +0600

    Add home page, video page design and routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import VideoGrid from "../components/grid/VideoGrid";
+import Tags from "../components/tags/Tags";
+
+const Home = () => {
+  return (
+    <div>
+      <Tags />
+      <VideoGrid />
+    </div>
+  );
+};
+
+export default Home;
