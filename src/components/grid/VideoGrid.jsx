208424f2044aba56a5a5ba00788ffc1bad4fd4a5
Hasibur Rahman <hasiburdev@gmail.com>
2023-02-03 12:47:26 +0600

    add redux toolkit

--- src/components/grid/VideoGrid.jsx
+++ src/components/grid/VideoGrid.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchVideos } from "../../rtk/features/videos/videosSlice";
 import VideoGridItems from "./VideoGridItems";
+import Loading from "../ui/Loading";
 
 const VideoGrid = () => {
+  const dispatch = useDispatch();
+  const { videos, isLoading, isError, error } = useSelector(
+    (state) => state.videos
+  );
+  useEffect(() => {
+    dispatch(fetchVideos());
+  }, [dispatch]);
+
+  let content;
+  if (isLoading) content = <Loading />;
+
   return (
     <section className="pt-12">
       <section className="pt-12">
         <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
           <VideoGridItems />
-          {/* <!-- error section
-                <div className="col-span-12">some error happened</div> --> */}
         </div>
       </section>
     </section>
